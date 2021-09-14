b89861cc01d232225d4b844961cde26714fa0bd4
Jussi Savoranta <jussi.savoranta@savelan.fi>
2021-09-14 09:05:35 +0300

    Finished MouseListener lesson

--- src/MouseListener/MyFrame.java
+++ src/MouseListener/MyFrame.java
@@ -8,19 +8,33 @@ import java.awt.event.MouseListener;
 public class MyFrame extends JFrame implements MouseListener {
 
     JLabel label;
+    ImageIcon smile;
+    ImageIcon nervous;
+    ImageIcon pain;
+    ImageIcon dizzy;
 
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
 
         label = new JLabel();
-        label.setBounds(0,0,100,100);
-        label.setBackground(Color.lightGray);
-        label.setOpaque(true);
-        this.addMouseListener(this);
+//        label.setBounds(0,0,100,100);
+//        label.setBackground(Color.lightGray);
+//        label.setOpaque(true);
+        label.addMouseListener(this);
+
+        smile = new ImageIcon("C:\\Users\\JussiSavoranta\\code\\java\\training\\SwingGUI\\src\\MouseListener\\bigSmile.png");
+        nervous = new ImageIcon("C:\\Users\\JussiSavoranta\\code\\java\\training\\SwingGUI\\src\\MouseListener\\EyeRoll.png");
+        pain = new ImageIcon("C:\\Users\\JussiSavoranta\\code\\java\\training\\SwingGUI\\src\\MouseListener\\frustration.png");
+        dizzy = new ImageIcon("C:\\Users\\JussiSavoranta\\code\\java\\training\\SwingGUI\\src\\MouseListener\\dizzy.png");
+
+        label.setIcon(smile);
+        this.add(label);
 
         this.add(label);
-        this.setLayout(null);
+        this.setLayout(new FlowLayout());
+        this.pack();
+        this.setLocationRelativeTo(null); // will make your frame appear in the middle of computer screen
         this.setVisible(true);
     }
 
@@ -35,7 +49,8 @@ public class MyFrame extends JFrame implements MouseListener {
     public void mousePressed(MouseEvent e) {
         // Invoked when a mouse button has been pressed on a component
         //System.out.println("You pressed the mouse");
-        label.setBackground(Color.yellow);
+//        label.setBackground(Color.yellow);
+        label.setIcon(pain);
 
     }
 
@@ -43,7 +58,8 @@ public class MyFrame extends JFrame implements MouseListener {
     public void mouseReleased(MouseEvent e) {
         // Invoked when a mouse button has been released on a component
         //System.out.println("You released the mouse");
-        label.setBackground(Color.green);
+//        label.setBackground(Color.green);
+        label.setIcon(dizzy);
 
     }
 
@@ -51,7 +67,8 @@ public class MyFrame extends JFrame implements MouseListener {
     public void mouseEntered(MouseEvent e) {
         // Invoked when the mouse enters a component
         System.out.println("You enterd the label");
-        label.setBackground(Color.blue);
+//        label.setBackground(Color.blue);
+        label.setIcon(nervous);
 
     }
 
@@ -59,7 +76,8 @@ public class MyFrame extends JFrame implements MouseListener {
     public void mouseExited(MouseEvent e) {
         // Invoked when the mouse exits a component
         System.out.println("You exited the label");
-        label.setBackground(Color.red);
+//        label.setBackground(Color.red);
+        label.setIcon(smile);
 
     }
 }
